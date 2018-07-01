3fbd3d0cf9b80245aea290cbcf4962f4597f7ebc
Love-zj <Love-zj@DESKTOP-ACS5A7B.lan>
2018-07-01 22:48:40 +0800

    2 更新

--- src/main/java/com/myt/domo/Apple.java
+++ src/main/java/com/myt/domo/Apple.java
@@ -1,5 +1,7 @@
 package com.myt.domo;
 
 public class Apple {
-
+	private void syso() {
+		System.out.println("111");
+	}
 }
--- src/main/java/com/myt/domo/Happy.java
+++ src/main/java/com/myt/domo/Happy.java
@@ -1,5 +1,7 @@
 package com.myt.domo;
 
 public class Happy {
-
+	private void syso() {
+		System.out.println("111");
+	}
 }
